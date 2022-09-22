55f5d051c074e23bdd726b65290237bf671e3271
gracedward <edward.chung.designer@gmail.com>
2022-09-22 16:11:02 +0100

    Refactor: for Netlify deploy

--- server/src/api.js
+++ server/src/api.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const serverless = require("serverless-http");
 
+const axios = require("axios");
+
 const app = express();
 
 app.use(express.json());
 
-const settings = { method: "Get" };
+app.get("/.netlify/functions/api/test", (request, response) => {
+  return response.send("hello");
+});
 
 app.get(
   "/.netlify/functions/api/:from/to/:to/:timeOffset",
@@ -16,12 +20,14 @@ app.get(
     const URL1 = `https://huxley2.azurewebsites.net/departures/${request.params.from}${to}/20?expand=true&timeOffset=${timeOffset}&timeWindow=120`;
     const URL2 = `https://huxley2.azurewebsites.net/arrivals/${request.params.to}/from/${request.params.from}/20?expand=true&timeOffset=${timeOffset2}&timeWindow=120`;
 
-    fetch(URL1, settings)
-      .then((response) => response.json())
+    axios
+      .get(URL1)
+      .then((response) => response.data)
       .then((data) => {
         if (request.params.to !== "NIL") {
-          fetch(URL2, settings)
-            .then((response2) => response2.json())
+          axios
+            .get(URL2)
+            .then((response2) => response2.data)
             .then((data2) => {
               const trainServices = data.trainServices?.map((train) => {
                 const dPlatform =

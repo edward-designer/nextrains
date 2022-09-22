a2ccf8b73ecae0282acf3f61081542f818433476
gracedward <edward.chung.designer@gmail.com>
2022-09-22 16:37:12 +0100

    Fix: enables client-side routing

--- src/components/TrainList/Tags.tsx
+++ src/components/TrainList/Tags.tsx
@@ -8,7 +8,7 @@ type TTag = {
 const Tag = ({ children, className }: TTag) => {
   return (
     <span
-      className={`inline-block text-xs w-16 h-[20px] relative text-reverse-color pl-4 py-[3px] ${className} 
+      className={`inline-block text-xs min-w-16 h-[20px] relative text-reverse-color pl-4 py-[3px] ${className} 
         after:absolute after:left-[100%] after:top-0 after:w-0 after:h-0 after:border-t-[10px] after:border-t-transparent after:border-l-[10px] after:border-b-[10px] after:border-b-transparent
         `}
     >

57a2c200174242e649f10f49544ceaf18d04012e
gracedward <edward.chung.designer@gmail.com>
2022-12-27 15:40:23 +0000

    debug: clear subsequent selected train legs

--- src/components/TrainList/TrainRowContainer.tsx
+++ src/components/TrainList/TrainRowContainer.tsx
@@ -75,6 +75,7 @@ const TrainRowContainer = ({
           tempSelectedTrains.delete(leg + i);
         }
         setSelectedTrains(tempSelectedTrains);
+        console.log(tempSelectedTrains);
       } else {
         const tempSelectedTrains = new Map(selectedTrains);
         tempSelectedTrains.set(leg, trainDetails);
